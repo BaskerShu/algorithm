91b77eaf1eb1d68c1ea9d8d83f05bad0335d7272
BaskerShu <yangshuzhi@outlook.com>
2020-08-10 08:16:18 +0800

    refactor

--- src/BinaryTree/LevelOrder.java
+++ src/BinaryTree/LevelOrder.java
@@ -6,24 +6,8 @@ import java.util.List;
 import java.util.Queue;
 
 /**
- * 429. N叉树的层序遍历
+ * N叉树的前序遍历
  */
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-}
-
 public class LevelOrder {
 
     // 广度搜索
