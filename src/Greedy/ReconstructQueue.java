59ecdb476cbb06fd69221dac8d62ada7ff0f9c57
BaskerShu <yangshuzhi@outlook.com>
2020-11-25 23:20:58 +0800

    406. 根据身高重建队列

---
+++ src/Greedy/ReconstructQueue.java
@@ -0,0 +1,60 @@
+package Greedy;
+
+import java.util.*;
+
+/**
+ * 406. 根据身高重建队列
+ */
+public class ReconstructQueue {
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] person1, int[] person2) {
+                if (person1[0] != person2[0]) {
+                    return person2[0] - person1[0];
+                } else {
+                    return person1[1] - person2[1];
+                }
+            }
+        });
+        List<int[]> ans = new ArrayList<int[]>();
+        for (int[] person : people) {
+            ans.add(person[1], person);
+        }
+        return ans.toArray(new int[ans.size()][]);
+    }
+
+    public int[][] reconstructQueue2(int[][] people) {
+        Arrays.sort(people, new Comparator<int[]>() {
+            public int compare(int[] person1, int[] person2) {
+                if (person1[0] != person2[0]) {
+                    return person1[0] - person2[0];
+                } else {
+                    return person2[1] - person1[1];
+                }
+            }
+        });
+        int n = people.length;
+        int[][] ans = new int[n][];
+        for (int[] person : people) {
+            int spaces = person[1] + 1;
+            for (int i = 0; i < n; ++i) {
+                if (ans[i] == null) {
+                    --spaces;
+                    if (spaces == 0) {
+                        ans[i] = person;
+                        break;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        var obj = new ReconstructQueue();
+        int[][] people = {{7,0}, {4,4}, {7,1}, {5,0}, {6,1}, {5,2}};
+
+        var res = obj.reconstructQueue2(people);
+        System.out.println(Arrays.toString(res));
+    }
+}
