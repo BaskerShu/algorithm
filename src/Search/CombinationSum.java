6b2444915af35277bec979c1a77736fa49f9d6c2
BaskerShu <yangshuzhi@outlook.com>
2021-01-10 18:48:03 +0800

    39. 组合总和

--- src/Search/CombinationSum.java
+++ src/Search/CombinationSum.java
@@ -1,9 +1,6 @@
 package Search;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 // 39. 组合总和
 public class CombinationSum {
@@ -52,6 +49,33 @@ public class CombinationSum {
             curr.addLast(candidates[i]);
             combinationSum2(candidates, target - candidates[i], i + 1, curr, ans);
             curr.removeLast();
+        }
+    }
+
+
+    public List<List<Integer>> combinationSum3(int[] candidates, int target) {
+        List<List<Integer>> ans = new LinkedList<>();
+        List<Integer> curr = new LinkedList<>();
+
+        Arrays.sort(candidates);
+        dfs(target, candidates, 0, 0, curr, ans);
+        return ans;
+    }
+
+    private void dfs(int target, int[] candidates, int l, int currNum, List<Integer> curr, List<List<Integer>> ret) {
+        if (currNum == target) {
+            ret.add(new LinkedList<>(curr));
+        }
+        if (currNum + candidates[l] > target) {
+            return;
+        }
+
+        for (int i = l; i < candidates.length; i++) {
+            curr.add(candidates[i]);
+            currNum += candidates[i];
+            dfs(target, candidates, i, currNum, curr, ret);
+            curr.remove(curr.size() - 1);
+            currNum -= candidates[i];
         }
     }
 }
